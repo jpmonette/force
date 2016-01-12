b0d500652acc7b1a1ba6ce57c13e2994b24477ef
Jean-Philippe Monette <contact@jpmonette.net>
2016-01-12 23:50:16 +0000

    adding executeAnonymous (tooling api)

---
+++ examples/tooling/executeAnonymous/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/jpmonette/force"
+	"golang.org/x/oauth2"
+)
+
+func main() {
+
+	conf := &oauth2.Config{
+		ClientID:     os.Getenv("SFCLIENTID"),
+		ClientSecret: os.Getenv("SFCLIENTSECRET"),
+		Endpoint: oauth2.Endpoint{
+			TokenURL: os.Getenv("SFTOKENURL"),
+		},
+	}
+
+	token, _ := conf.PasswordCredentialsToken(oauth2.NoContext, os.Getenv("SFUSERNAME"), os.Getenv("SFPASSWORD"))
+
+	instanceUrl, _ := token.Extra("instance_url").(string)
+	client := conf.Client(oauth2.NoContext, token)
+
+	c, _ := force.NewClient(client, instanceUrl)
+
+	result, _ := c.Tooling.ExecuteAnonymous("System.debug('test');")
+
+	fmt.Println(result.Success)
+	// Output: true
+}
--- tooling.go
+++ tooling.go
@@ -8,6 +8,26 @@ type ToolingService struct {
 	client *Client
 }
 
+// ExecuteAnonymousResult specifies information about whether or not the
+// compile and run of the code was successful
+type ExecuteAnonymousResult struct {
+	Column              int    `json:"column"`
+	CompileProblem      string `json:"compileProblem"`
+	Compiled            bool   `json:"compiled"`
+	ExceptionMessage    string `json:"exceptionMessage"`
+	ExceptionStackTrace string `json:"exceptionStackTrace"`
+	Line                int    `json:"line"`
+	Success             bool   `json:"success"`
+}
+
+// ExecuteAnonymous executes the specified block of Apex anonymously and
+// returns the result.
+func (c *ToolingService) ExecuteAnonymous(apex string) (result ExecuteAnonymousResult, err error) {
+	req, err := c.client.NewRequest("GET", "/tooling/executeAnonymous/?anonymousBody="+url.QueryEscape(apex), nil)
+	err = c.client.Do(req, &result)
+	return
+}
+
 // Query executes a query against a Tooling API object and returns data that
 // matches the specified criteria.
 func (c *ToolingService) Query(soql string, v interface{}) (err error) {

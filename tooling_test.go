1ccfa46821f63e46988a98232eaca279987a40b8
Jean-Philippe Monette <contact@jpmonette.net>
2016-01-13 00:28:09 +0000

    adding test for query (tooling)

--- tooling_test.go
+++ tooling_test.go
@@ -23,3 +23,28 @@ func TestExecuteAnonymous(t *testing.T) {
 	assert.Nil(t, err)
 	assert.Equal(t, true, result.Success)
 }
+
+// QueryResponse is a Tooling API Query response object.
+type QueryResponse struct {
+	Records []struct {
+		FullName string `json:"FullName"`
+	} `json:"records"`
+}
+
+func TestQuery(t *testing.T) {
+	setup()
+	defer teardown()
+
+	mux.HandleFunc("/services/data/v34.0/tooling/query/", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		fmt.Fprint(w, `{"done":true,"entityTypeName":"Profile","queryLocator":"","records":[{"FullName":"Admin","attributes":{"type":"Profile","url":"/services/data/v34.0/tooling/sobjects/Profile/00N20000009gIZmEAM"}}],"size":1,"totalSize":1}`)
+	})
+
+	var result QueryResponse
+
+	err := client.Tooling.Query("SELECT FullName FROM Profile", &result)
+
+	assert.NotNil(t, result)
+	assert.Nil(t, err)
+	assert.Equal(t, "Admin", result.Records[0].FullName)
+}

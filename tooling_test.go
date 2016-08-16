13a7aa0e0f26bfabbb3a76d7bf544abc7a157638
Jean-Philippe Monette <contact@jpmonette.net>
2016-08-16 15:21:29 -0400

    Update API version in Unit Tests

--- tooling_test.go
+++ tooling_test.go
@@ -12,7 +12,7 @@ func TestDescribeGlobal(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc("/services/data/v34.0/tooling/sobjects/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/services/data/"+apiVersion+"/tooling/sobjects/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, `{"encoding":"UTF-8","maxBatchSize":1,"sobjects":[{"activateable":false,"createable":true,"custom":false,"customSetting":false,"deletable":true,"deprecatedAndHidden":false,"feedEnabled":false,"keyPrefix":"01p","label":"Apex Class","labelPlural":"Apex Classes","layoutable":false,"mergeable":false,"name":"ApexClass","queryable":true,"replicateable":true,"retrieveable":true,"searchable":true,"triggerable":false,"undeletable":false,"updateable":true,"urls":{"rowTemplate":"/services/data/v34.0/tooling/sobjects/ApexClass/{ID}","describe":"/services/data/v34.0/tooling/sobjects/ApexClass/describe","sobject":"/services/data/v34.0/tooling/sobjects/ApexClass"}}]}`)
 	})
@@ -29,7 +29,7 @@ func TestExecuteAnonymous(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc("/services/data/v34.0/tooling/executeAnonymous/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/services/data/"+apiVersion+"/tooling/executeAnonymous/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, `{"column":-1,"compileProblem":null,"compiled":true,"exceptionMessage":null,"exceptionStackTrace":null,"line":-1,"success":true}`)
 	})
@@ -52,7 +52,7 @@ func TestQuery(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc("/services/data/v34.0/tooling/query/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/services/data/"+apiVersion+"/tooling/query/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, `{"done":true,"entityTypeName":"Profile","queryLocator":"","records":[{"FullName":"Admin","attributes":{"type":"Profile","url":"/services/data/v34.0/tooling/sobjects/Profile/00N20000009gIZmEAM"}}],"size":1,"totalSize":1}`)
 	})
@@ -70,7 +70,7 @@ func TestRunTests(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc("/services/data/v34.0/tooling/runTestsSynchronous/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/services/data/"+apiVersion+"/tooling/runTestsSynchronous/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, `{"apexLogId":"","codeCoverage":[{"dmlInfo":[],"id":"01q20000000AmAWAA0","locationsNotCovered":[{"column":0,"line":2,"numExecutions":0,"time":-1},{"column":0,"line":5,"numExecutions":0,"time":-1},{"column":0,"line":6,"numExecutions":0,"time":-1}],"methodInfo":[],"name":"Events","namespace":"","numLocations":3,"numLocationsNotCovered":3,"soqlInfo":[],"soslInfo":[],"type":"Trigger"}],"codeCoverageWarnings":[{"id":"01q20000000AmAWAA0","message":"Test coverage of selected Apex Trigger is 0%, at least 1% test coverage is required","name":"Events","namespace":""},{"id":"01q20000000AmAWAA0","message":"Average test coverage across all Apex Classes and Triggers is 0%, at least 75% test coverage is required.","name":"","namespace":""}],"failures":[],"numFailures":0,"numTestsRun":1,"successes":[{"id":"01p20000004dwL0AAI","methodName":"test","name":"Tests_T","namespace":"","seeAllData":"","time":138}],"totalTime":140}`)
 	})
@@ -87,7 +87,7 @@ func TestRunTestsAsynchronous(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc("/services/data/v37.0/tooling/runTestsAsynchronous/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/services/data/"+apiVersion+"/tooling/runTestsAsynchronous/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, `"707E000004Btvxf"`)
 	})
@@ -112,7 +112,7 @@ func TestSearch(t *testing.T) {
 	setup()
 	defer teardown()
 
-	mux.HandleFunc("/services/data/v34.0/tooling/search/", func(w http.ResponseWriter, r *http.Request) {
+	mux.HandleFunc("/services/data/"+apiVersion+"/tooling/search/", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
 		fmt.Fprint(w, `[{"attributes":{"type":"EntityDefinition","url":"/services/data/v34.0/tooling/sobjects/EntityDefinition/4ie000000000005AAA"},"Id":"4ie000000000005AAA","DurableId":"AccountContactRole"},{"attributes":{"type":"EntityDefinition","url":"/services/data/v34.0/tooling/sobjects/EntityDefinition/4ie000000000006AAA"},"Id":"4ie000000000006AAA","DurableId":"AccountCleanInfo"}]`)
 	})
